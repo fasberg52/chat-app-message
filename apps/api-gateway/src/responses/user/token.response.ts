81449a210ca48571072ce552afabaf6937e5bce2
mohammadreza.taherian <taherian.1993@gmail.com>
2025-01-16 14:09:11 +0330

    fixed response for token result

--- apps/api-gateway/src/responses/user/token.response.ts
+++ apps/api-gateway/src/responses/user/token.response.ts
@@ -2,12 +2,22 @@ import { BaseResponse } from '@app/common/responses/base.response';
 import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
 
 @ApiExtraModels()
-export class TokenResponse extends BaseResponse {
+export class TokenResult {
   @ApiProperty()
   accessToken: string;
 
-  constructor(token: string) {
+  constructor(accessToken: string) {
+    this.accessToken = accessToken;
+  }
+}
+
+@ApiExtraModels()
+export class TokenResponse extends BaseResponse {
+  @ApiProperty({ type: TokenResult })
+  result: TokenResult;
+
+  constructor(accessToken: string) {
     super();
-    this.accessToken = token;
+    this.result = new TokenResult(accessToken);
   }
 }
